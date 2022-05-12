a3795cf7877cb1163debb79e6171ac6b0d84cf50
KimYeounSu <idkim9797@naver.com>
2022-05-13 00:54:31 +0900

    index 페이

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,51 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name); // "name"은 key, name은 파라미터의 String name
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        // json 방식으로 key-value값을 전달
+        // 최근에는 거의 json방식을 많이 사용
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        // getter setter 단축키 : Alt+Insert
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}

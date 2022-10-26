6e4d227a039ad5abbb42c65ba61bf29f9b1c90d5
KimYeounSu <idkim9797@naver.com>
2022-10-27 06:41:16 +0900

    멤버 리포지토리

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 // Alt + Enter -> implement method -> 전부 ok
-@Repository
+
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();

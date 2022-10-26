ae33eae90363d2ba9ca1a2d06b5c5c106bee9bca
KimYeounSu <idkim9797@naver.com>
2022-10-27 06:40:54 +0900

    스프링 빈 수동 등

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // 수동으로 스프링 빈에 빈 생성
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
